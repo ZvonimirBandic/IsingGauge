55de8b79126c29fad1f6054c00872d1bdd7da563
Zvonimir Bandic <Zvonimir.bandic@gmail.com>
2021-02-06 21:32:08 -0800

    Update batch_mc.sh
    adding mkdir for folder results which is expected by top module

--- batch_mc.sh
+++ batch_mc.sh
@@ -1,4 +1,7 @@
 echo "batch run..."
 
+mkdir results # need to have results directory where IsingGaugeTop places CSV results files
+
+# this is an example run
 
 julia IsingGaugeTop.jl 12 3 0 0.5 3 0.05 50000
